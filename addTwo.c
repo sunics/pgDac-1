d265b036b09006b60164c9211b09456628ba9223
Abhishek <abhishek.patel18596@gmail.com>
2021-10-06 15:00:36 +0530

    first copy of my file

--- addTwo.c
+++ addTwo.c
@@ -4,7 +4,7 @@ void addTwo()
 	int n1,n2;
 	printf("Enter two integers ");
 	scanf("%d,%d",&n1,&n2);
-	printf("Sum of the two number is %d",n1+n2);
+	printf("Sum of %d and %d is %d",n1,n2,n1+n2);
 }
 
 void main()
